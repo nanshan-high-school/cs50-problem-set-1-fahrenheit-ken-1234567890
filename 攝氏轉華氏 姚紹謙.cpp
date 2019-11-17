7a93f45dfbc37d209ba8bb94908bb56259eb9bc3
ken-1234567890 <56679717+ken-1234567890@users.noreply.github.com>
2019-11-17 16:40:57 +0800

    Create 攝氏轉華氏 姚紹謙.cpp

---
+++ 攝氏轉華氏 姚紹謙.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "請輸入溫度";
+    double f;
+    cin >> f;
+    double c;
+    c = 9 * f / 5 - 32;
+    cout << c; // 269 43 姚紹謙
+}
